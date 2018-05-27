7485d3d13b2ede952803f84a10f84186d75a52b6
李浩 <757937784@qq.com>
2018-05-27 22:02:18 +0800

    Core

---
+++ NiuBang.Core/Infrastructure/AppRoleManager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace NiuBang.Core.Infrastructure
+{
+    public class AppRoleManager:RoleManager<Domain.AppRole>
+    {
+        public AppRoleManager(IRoleStore<Domain.AppRole,string> store):base (store)
+        {
+
+        }
+    }
+}
---
+++ NiuBang.Core/Infrastructure/AppUerManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace NiuBang.Core.Infrastructure
+{
+    public class AppUerManager : UserManager<Domain.AppUser>
+    {
+        public AppUerManager(IUserStore<Domain.AppUser> store) : base(store)
+        {
+
+        }
+
+    }
+}
